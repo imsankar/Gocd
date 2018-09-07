a91c639cea612750b69cf8f493d96e7cce6d899b
imsankar <sankar.zabu@gmail.com>
2018-09-07 13:55:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 platform=$1
 command=$2
+echo $platform
+echo $command
 case $platform in
   maven)
-    echo $command
+   $command
   ;;
   dotnet)
-    echo $command
+   $command
   ;;
   *)
     echo "invalid tf section $2" >&2
     exit 1
   ;;
 esac
- 
+
 
  
